db37859c16a648f234efadc300e82ec95714f990
Monil Santilal <Monza972@users.noreply.github.com>
2013-12-03 02:40:17 +0000

    Gallery and Camera working at full pelt!

--- src/pac/instabuildings/android/NewPostActivity.java
+++ src/pac/instabuildings/android/NewPostActivity.java
@@ -1,29 +1,52 @@
 package pac.instabuildings.android;
 
-import android.net.Uri;
-import android.os.Bundle;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class NewPostActivity extends Activity {
 	
+	private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
 	private static final int Select_Picture = 1;
-	Button gallery;
-	
+	private static final int MEDIA_TYPE_IMAGE = 1;
+	ImageView gallery;
+	private ImageView imageView;
+	private Bitmap bitmap;
+	//image output folder
+	private static final String IMAGE_DIRECTORY_NAME = "InstaBuildings";
+	private Uri fileUri; //url to store store image
+	private ImageView imgPreview;
+	private Button btnCapturePicture;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_post);
 		getActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_background));
-
-		gallery = (Button) findViewById(R.id.button1);
+		gallery = (ImageView) findViewById(R.id.result);
 		gallery.setOnClickListener(new OnClickListener() {
 		
 		public void onClick(View v)
@@ -34,26 +57,82 @@ public class NewPostActivity extends Activity {
 			startActivityForResult(gallery.createChooser(gallery, "Select Picture"), Select_Picture);
 		}
 	});
+		imageView = (ImageView) findViewById(R.id.result);
+		imageView.bringToFront();
+		
+		imgPreview = (ImageView) findViewById(R.id.imgPreview);
+		btnCapturePicture = (Button) findViewById(R.id.btCapturePicture);
+		btnCapturePicture.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				// TODO Auto-generated method stub
+				captureImage();
+			}
+		});
+		
+
+		
 		
 	}		
+
+	public void pickImage (View View)
+	{
+		Intent intent = new Intent();
+		intent.setType("image/*");
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+		intent.addCategory(Intent.CATEGORY_OPENABLE);
+		startActivityForResult(intent,Select_Picture);
+		
+	}
 	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data)
 	{
 		super.onActivityResult(requestCode, resultCode, data);
-		switch(requestCode) 
+		if (requestCode == Select_Picture && resultCode == Activity.RESULT_OK)
 		{
-		case Select_Picture: // our request code
-			if(resultCode == Activity.RESULT_OK)
-			{ // result was ok
-				Uri selectedImage = data.getData();
-				Intent i = new Intent(getApplicationContext(),ImageView.class);
-				
-				i.putExtra("PICTURE_LOCATION", selectedImage.toString());
-				startActivity(i);
+			try 
+			{
+				if (bitmap != null)
+				{
+					bitmap.recycle();
+				}
+				InputStream stream = getContentResolver().openInputStream(data.getData());
+				bitmap = BitmapFactory.decodeStream(stream);
+				stream.close();
+				imageView.setImageBitmap(bitmap);
+			} 
+			catch (FileNotFoundException e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}	
+		if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE)
+		{
+			if (resultCode == RESULT_OK)
+			{
+				//successfully captured image
+				//display it in imageview
+				previewCaptureImage();
+			}
+			else if (resultCode == RESULT_CANCELED)
+			{
+				Toast.makeText(getApplicationContext(), "User cancelled image capture", Toast.LENGTH_SHORT).show();
+			}
+			else
+			{
+				Toast.makeText(getApplicationContext(), "Sorry! Failed to capture image", Toast.LENGTH_SHORT).show();
+			}	
 		}
+		super.onActivityResult(requestCode, resultCode, data);
 	}
-	
+		
 	@Override
 	
 	
@@ -84,7 +163,97 @@ public class NewPostActivity extends Activity {
 		return true;
 	}
 
-    //---------------------- ActionBar actions --------------------//
+	private boolean isDeviceSupportCamera()
+	{
+		if (getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+    
+	private void captureImage()
+	{
+		Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+		camera.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+		startActivityForResult(camera, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+	}
+	
+	private void previewCaptureImage()
+	{
+		try 
+		{
+			imgPreview.setVisibility(View.VISIBLE);
+			BitmapFactory.Options options = new BitmapFactory.Options();
+			options.inSampleSize = 8;
+			final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(),options);
+			imgPreview.setImageBitmap(bitmap);
+		} 
+		catch (Exception e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState)
+	{
+		super.onSaveInstanceState(outState);
+		outState.putParcelable("file_uri", fileUri);
+	}
+	
+	protected void onRestoreInstanceState(Bundle savedInstanceState)
+	{
+		super.onRestoreInstanceState(savedInstanceState);
+		fileUri = savedInstanceState.getParcelable("file_uri");
+	}
+
+	public Uri getOutputMediaFileUri(int type)
+	{
+		return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	private static File getOutputMediaFile(int type)
+	{
+
+		// External sdcard location
+		File mediaStorageDir = new File(
+				Environment
+				.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				IMAGE_DIRECTORY_NAME);
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+						+ IMAGE_DIRECTORY_NAME + " directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+				Locale.getDefault()).format(new Date());
+		File mediaFile;
+		if (type == MEDIA_TYPE_IMAGE) 
+		{
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "IMG_" + timeStamp + ".jpg");
+		}
+		else {
+			return null;
+		}
+
+		return mediaFile;
+	}
+
+	//---------------------- ActionBar actions --------------------//
 	
 	public void sendMessage()
 	//Method used to create new posts for the "InstaBuildings" network
